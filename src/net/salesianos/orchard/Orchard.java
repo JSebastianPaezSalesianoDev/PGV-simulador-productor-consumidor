b61c58461f0d02355823b97e2833d2aac74f75ae
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-11-24 18:23:15 +0000

    Feat(orchard):Creacion de atributos y metodo añadir verdura en orchard

--- src/net/salesianos/orchard/Orchard.java
+++ src/net/salesianos/orchard/Orchard.java
@@ -1,5 +1,22 @@
 package net.salesianos.orchard;
 
-public class Orchard {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Orchard {
+private int capacidad;
+    private Queue<String> verduras = new LinkedList<>();
+
+    public Orchard(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public synchronized void añadirVerdura(String verdura, int farmerId) throws InterruptedException {
+        while (verduras.size() == capacidad) {
+            wait();
+        }
+        verduras.add(verdura);
+        System.out.println("Granjero " + farmerId + " ha producido: " + verdura);
+        notifyAll();
+    }
 }
