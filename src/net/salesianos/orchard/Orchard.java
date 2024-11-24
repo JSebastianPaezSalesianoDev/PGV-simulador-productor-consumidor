106c4607310fee129c0b3624b03322f9e6ac1203
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-11-24 20:53:47 +0000

    overwrite orchid

--- src/net/salesianos/orchard/Orchard.java
+++ src/net/salesianos/orchard/Orchard.java
@@ -1,22 +1,23 @@
 package net.salesianos.orchard;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Orchard {
-private int capacidad;
-    private Queue<String> verduras = new LinkedList<>();
+    private int capacidad;
+    private List<String> verduras;
 
     public Orchard(int capacidad) {
         this.capacidad = capacidad;
+        this.verduras = new ArrayList<>(capacidad);
     }
 
-    public synchronized void añadirVerdura(String verdura, String farmerId) throws InterruptedException {
+    public synchronized void añadirVerdura(String verdura, String farmerName) throws InterruptedException {
         while (verduras.size() == capacidad) {
             wait();
         }
         verduras.add(verdura);
-        System.out.println("Granjero " + farmerId + " ha producido: " + verdura);
+        System.out.println("Granjero " + farmerName + " ha producido: " + verdura);
         notifyAll();
     }
 
@@ -24,19 +25,16 @@ private int capacidad;
         while (verduras.isEmpty()) {
             wait();
         }
-        String verdura = verduras.poll();
+        String verdura = verduras.remove(0);
         System.out.println("Cliente " + customerId + " ha consumido: " + verdura);
         notifyAll();
     }
 
-
     public boolean estaLleno() {
-        return verduras.size() == capacidad; 
+        return verduras.size() == capacidad;
     }
-
 
     public boolean estaVacio() {
-        return verduras.isEmpty(); 
+        return verduras.isEmpty();
     }
-
 }
